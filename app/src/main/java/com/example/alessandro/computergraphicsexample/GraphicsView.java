ec3ac5c543a26869785c8f7f4bd04479ecadf3a6
Andrea Torlaschi <runnerway@gmail.com>
2015-03-26 17:09:22 +0100

    Implementata prospettiva e aggiunto piano

--- app/src/main/java/com/example/alessandro/computergraphicsexample/GraphicsView.java
+++ app/src/main/java/com/example/alessandro/computergraphicsexample/GraphicsView.java
@@ -23,15 +23,17 @@ import shadow.math.SFMatrix3f;
 import shadow.math.SFTransform3f;
 
 import static android.opengl.GLES20.glViewport;
-import static android.opengl.Matrix.orthoM;
+import static android.opengl.Matrix.frustumM;
+import static android.opengl.Matrix.multiplyMM;
+import static android.opengl.Matrix.setLookAtM;
 
 /**
  * Created by Alessandro on 13/03/15.
  */
 public class GraphicsView extends GLSurfaceView {
 
+    private final float[] resultMatrix = new float[16];
     private Context context;
-    private float[] projection = new float[16];
 
     public GraphicsView(Context context) {
         super(context);
@@ -43,7 +45,7 @@ public class GraphicsView extends GLSurfaceView {
 
     public class GraphicsRenderer implements Renderer {
 
-        private Node node;
+        private Node node, groundNode, wallNode;
         private float t = 0;
         private ShadingProgram program;
 
@@ -55,54 +57,67 @@ public class GraphicsView extends GLSurfaceView {
 
             //Step 2 : load Textures
             int textureModel = SFOGLTextureModel.generateTextureObjectModel(SFImageFormat.RGB, GLES20.GL_REPEAT, GLES20.GL_REPEAT, GLES20.GL_LINEAR, GLES20.GL_LINEAR);
-            BitmapTexture texture = BitmapTexture.loadBitmapTexture(BitmapFactory.decodeResource(context.getResources(), R.drawable.paddedroomtexture01), textureModel);
+            BitmapTexture texture = BitmapTexture.loadBitmapTexture(BitmapFactory.decodeResource(context.getResources(), R.drawable.paddedroom_texture_01), textureModel);
             texture.init();
+            BitmapTexture groundTexture = BitmapTexture.loadBitmapTexture(BitmapFactory.decodeResource(context.getResources(), R.drawable.ground_texture_01), textureModel);
+            groundTexture.init();
 
             //Step 3 : create a Material (materials combine shaders+textures+shading parameters)
             Material material = new Material(program);
             material.getTextures().add(texture);
+            Material groundMaterial = new Material(program);
+            groundMaterial.getTextures().add(groundTexture);
 
             //Step 4: load a Geometry
             ArrayObject[] objects = ObjLoader.arrayObjectFromFile(context, "MonkeyTxN.obj");
+            ArrayObject[] groundObject = ObjLoader.arrayObjectFromFile(context, "Ground.obj");
 
             Mesh mesh = new Mesh(objects[0]);
             mesh.init();
+            Mesh groundMesh = new Mesh(groundObject[0]);
+            groundMesh.init();
 
             //Step 5: create a Model combining material+geometry
-            Model model1 = new Model();
-            model1.setRootGeometry(mesh);
-            model1.setMaterialComponent(material);
+            Model monkeyModel = new Model();
+            monkeyModel.setRootGeometry(mesh);
+            monkeyModel.setMaterialComponent(material);
+            Model groundModel = new Model();
+            groundModel.setRootGeometry(groundMesh);
+            groundModel.setMaterialComponent(groundMaterial);
 
             //Step 6: create a Node, that is a reference system where you can place your Model
             node = new Node();
-            node.setModel(model1);
-            node.getRelativeTransform().setPosition(0, 0, 0);
+            node.setModel(monkeyModel);
+            node.getRelativeTransform().setPosition(0, 0.5f, 0);
 
             Node anotherNode = new Node();
-            anotherNode.setModel(model1);
+            anotherNode.setModel(monkeyModel);
             anotherNode.getRelativeTransform().setPosition(1, 1, 0);
             anotherNode.getRelativeTransform().setMatrix(SFMatrix3f.getScale(0.3f, 0.2f, 0.1f));
             node.getSonNodes().add(anotherNode);
+
+            groundNode = new Node();
+            groundNode.setModel(groundModel);
+            groundNode.getRelativeTransform().setPosition(0, 0, 0);
         }
 
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height) {
             glViewport(0, 0, width, height);
-            final float aspectRatio = width > height ?
-                    (float) width / (float) height :
-                    (float) height / (float) width;
-            if (width > height) {
-                orthoM(projection, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
-            } else {
-                orthoM(projection, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
-            }
+
+            final float[] viewMatrix = new float[16];
+            final float[] projectionMatrix = new float[16];
+            setLookAtM(viewMatrix, 0, 0, +1, +2, 0, 0.5f, 1, 0, 1, 0);
+            float ratio = (float) width / height;
+            frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 1, 10);
+            multiplyMM(resultMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
         }
 
         @Override
         public void onDrawFrame(GL10 gl) {
-            SFOGLSystemState.cleanupColorAndDepth(1, 1, 0, 1);
+            SFOGLSystemState.cleanupColorAndDepth(0, 0, 1, 1);
 
-            program.setupProjection(projection);
+            program.setupProjection(resultMatrix);
 
             //Change the Node transform
             t += 0.01f;
@@ -113,12 +128,15 @@ public class GraphicsView extends GLSurfaceView {
             node.getRelativeTransform().setMatrix(matrix3f);
             node.updateTree(new SFTransform3f());
 
-            //Draw the node
-            node.draw();
+            //Change the Node transform
+            scaling = 1.0f;
+            matrix3f = SFMatrix3f.getScale(scaling, scaling, scaling);
+            groundNode.getRelativeTransform().setMatrix(matrix3f);
+            groundNode.updateTree(new SFTransform3f());
 
-            //int[] viewport=new int[4];
-            //GLES20.glGetIntegerv(GLES20.GL_VIEWPORT,viewport,0);
-            //Log.e("Graphics View Size", Arrays.toString(viewport));
+            //Draw nodes
+            node.draw();
+            groundNode.draw();
         }
     }
 }
