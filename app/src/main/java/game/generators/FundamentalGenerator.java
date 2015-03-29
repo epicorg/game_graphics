dc57930065334d59d2c389d44dc70ef6c9a4a306
Andrea Torlaschi <runnerway@gmail.com>
2015-03-29 22:58:38 +0200

    Refactoring

--- app/src/main/java/game/generators/FundamentalGenerator.java
+++ app/src/main/java/game/generators/FundamentalGenerator.java
@@ -1,8 +1,6 @@
 package game.generators;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.opengl.GLES20;
 
 import game.graphics.TextureKeeper;
 import objLoader.ObjLoader;
@@ -12,8 +10,6 @@ import sfogl.integration.Material;
 import sfogl.integration.Mesh;
 import sfogl.integration.Model;
 import sfogl.integration.ShadingProgram;
-import sfogl2.SFOGLTextureModel;
-import shadow.graphics.SFImageFormat;
 
 /**
  * Created by Andrea on 27/03/2015.
@@ -21,8 +17,7 @@ import shadow.graphics.SFImageFormat;
 public class FundamentalGenerator {
 
     public static Material getMaterial(Context context, ShadingProgram program, int textureId) {
-        int textureModel = SFOGLTextureModel.generateTextureObjectModel(SFImageFormat.RGB, GLES20.GL_REPEAT, GLES20.GL_REPEAT, GLES20.GL_LINEAR, GLES20.GL_LINEAR);
-        BitmapTexture texture = BitmapTexture.loadBitmapTexture(BitmapFactory.decodeResource(context.getResources(), textureId), textureModel);
+        BitmapTexture texture = TextureKeeper.getTexture(context, textureId);
         texture.init();
         Material material = new Material(program);
         material.getTextures().add(texture);
@@ -31,15 +26,14 @@ public class FundamentalGenerator {
     }
 
     public static Model getModel(Context context, ShadingProgram program, int texture_id, String obj) {
-        BitmapTexture texture = TextureKeeper.getTexture(context, texture_id);
         ArrayObject[] object = ObjLoader.arrayObjectFromFile(context, obj);
         Mesh mesh = new Mesh(object[0]);
         mesh.init();
-        Material material = new Material(program);
-        material.getTextures().add(texture);
+        Material material = getMaterial(context, program, texture_id);
         Model model = new Model();
         model.setRootGeometry(mesh);
         model.setMaterialComponent(material);
+
         return model;
     }
 
