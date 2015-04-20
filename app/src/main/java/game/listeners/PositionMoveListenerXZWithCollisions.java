d08a81d11c7c17a7c6d89bba9cc77d82c3a99c41
StevenDepa <stefano.depa91@gmail.com>
2015-04-21 00:10:09 +0200

    Refactoring PlayerStatus e PositionMoveListenerXZWithCollisions

--- app/src/main/java/game/listeners/PositionMoveListenerXZWithCollisions.java
+++ app/src/main/java/game/listeners/PositionMoveListenerXZWithCollisions.java
@@ -1,6 +1,5 @@
 package game.listeners;
 
-import game.physics.CollisionBox;
 import game.physics.CollisionMediator;
 import game.player.PlayerStatus;
 import shadow.math.SFMatrix3f;
@@ -12,8 +11,6 @@ import shadow.math.SFVertex3f;
 public class PositionMoveListenerXZWithCollisions implements PositionMoveListenerInterface {
 
     public static final String LOG_TAG = "PositionMoveListenerXZ";
-
-    private static final int NUMBER_OF_ANGLE_DIVISIONS = 50;
     private static float MOVE_SPEED = 3.00f; // Distance/s
 
     private PlayerStatus playerStatus;
@@ -26,44 +23,15 @@ public class PositionMoveListenerXZWithCollisions implements PositionMoveListene
 
     @Override
     public void move(float angleXZ, float angleYZ, long delta) {
-        SFVertex3f position=playerStatus.getCollisionBox().getPos(), originalPosition = new SFVertex3f(position);
 
         SFMatrix3f rotationMatrix = SFMatrix3f.getRotationY(angleXZ);
         SFVertex3f tempVertex = rotationMatrix.Mult(new SFVertex3f(playerStatus.getDirection().getX(), 0, playerStatus.getDirection().getZ()));
         tempVertex.normalize3f();
         tempVertex.mult(MOVE_SPEED * delta / 1000);
 
-        position.add3f(tempVertex);
+        playerStatus.move(tempVertex,cm);
 
-        CollisionBox box=cm.collide(playerStatus.getCollisionBox());
-        // Correct for obstacles
-        if (box!=null)
-            correctMotion(position, originalPosition, tempVertex, box);
-        box=cm.collide(playerStatus.getCollisionBox());
-        // Correct for junctions
-        if (box!=null)
-            correctMotion(position, originalPosition, tempVertex, box);
-        // Reset if blocked
-        if (cm.collide(playerStatus.getCollisionBox())!=null)
-            position.set(originalPosition);
-        else
-            playerStatus.getPosition().set(position);
     }
 
-    public void correctMotion(SFVertex3f v, SFVertex3f v0, SFVertex3f tempVertex, CollisionBox box){
-        SFVertex3f vertex;
-        for (int i = 0; i < NUMBER_OF_ANGLE_DIVISIONS; i++) {
-            for (int j = -1; j < 2; j+=2) {
-                SFMatrix3f rotationMatrix = SFMatrix3f.getRotationY(i*j*(float)Math.PI/(2*NUMBER_OF_ANGLE_DIVISIONS));
-                vertex=new SFVertex3f(tempVertex);
-                vertex = rotationMatrix.Mult(vertex);
-                v.set(v0);
-                v.add(vertex);
-                if (!CollisionMediator.checkCollision(playerStatus.getCollisionBox(), box)){
-                    return;
-                }
-            }
-        }
-    }
 
 }
--- app/src/main/java/game/player/PlayerStatus.java
+++ app/src/main/java/game/player/PlayerStatus.java
@@ -1,6 +1,9 @@
 package game.player;
 
 import game.physics.Circle;
+import game.physics.CollisionBox;
+import game.physics.CollisionMediator;
+import shadow.math.SFMatrix3f;
 import shadow.math.SFVertex3f;
 
 /**
@@ -8,6 +11,7 @@ import shadow.math.SFVertex3f;
  */
 public class PlayerStatus {
 
+    private static final int NUMBER_OF_ANGLE_DIVISIONS = 50;
     private SFVertex3f position;
     private SFVertex3f direction;
     private Circle circle;
@@ -18,16 +22,47 @@ public class PlayerStatus {
         this.circle = circle;
     }
 
+    public void move(SFVertex3f motion, CollisionMediator cm) {
+        SFVertex3f circlePosition=circle.getPos(), originalPosition = new SFVertex3f(circlePosition);
+        circlePosition.add3f(motion);
+
+        CollisionBox box=cm.collide(circle);
+        // Correct for obstacles
+        if (box!=null)
+            correctMotion(circlePosition, originalPosition, motion, box);
+        box=cm.collide(circle);
+        // Correct for junctions
+        if (box!=null)
+            correctMotion(circlePosition, originalPosition, motion, box);
+        // Reset if blocked, else update effective position
+        if (cm.collide(circle)!=null)
+            circlePosition.set(originalPosition);
+        else
+            position.set(circlePosition);
+    }
+
+    private void correctMotion(SFVertex3f v, SFVertex3f v0, SFVertex3f tempVertex, CollisionBox box){
+        SFVertex3f vertex;
+        for (int i = 0; i < NUMBER_OF_ANGLE_DIVISIONS; i++) {
+            for (int j = -1; j < 2; j+=2) {
+                SFMatrix3f rotationMatrix = SFMatrix3f.getRotationY(i*j*(float)Math.PI/(2*NUMBER_OF_ANGLE_DIVISIONS));
+                vertex=new SFVertex3f(tempVertex);
+                vertex = rotationMatrix.Mult(vertex);
+                v.set(v0);
+                v.add(vertex);
+                if (!CollisionMediator.checkCollision(circle, box)){
+                    return;
+                }
+            }
+        }
+    }
+
     public SFVertex3f getPosition() {
         return position;
     }
 
     public SFVertex3f getDirection() {
         return direction;
-    }
-
-    public Circle getCollisionBox() {
-        return circle;
     }
 
 }
