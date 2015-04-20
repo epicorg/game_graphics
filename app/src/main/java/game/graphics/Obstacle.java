6dc5c96e4d435da8368ad616b7aa1897bc62feff
Andrea Torlaschi <runnerway@gmail.com>
2015-04-20 23:17:00 +0200

    Corretto sfondo

--- app/src/main/java/game/graphics/Obstacle.java
+++ app/src/main/java/game/graphics/Obstacle.java
@@ -10,37 +10,39 @@ import shadow.math.SFMatrix3f;
 
 /**
  * Ostacolo cilindrico.
+ *
  * @author Stefano De Pace
  */
-public class Obstacle implements MazeObject{
+public class Obstacle implements MazeObject {
 
     private Circle c;
     private int texture_id;
     private float height;
 
     /**
-     * Creau un nuovo Obstacle di dimensioni e texture dati.
-     * @param c Circle che costituisce la CollisionBox e determina le dimensioni nel piano xz.
-     * @param height altezza nella direzione y.
+     * Crea un nuovo Obstacle di dimensioni e texture dati.
+     *
+     * @param c          Circle che costituisce la CollisionBox e determina le dimensioni nel piano xz.
+     * @param height     altezza nella direzione y.
      * @param texture_id indice della texture nelle risorse.
      */
-    public Obstacle(Circle c, double height, int texture_id){
-        this.c=c;
-        this.texture_id=texture_id;
-        this.height=(float)height/2;
+    public Obstacle(Circle c, double height, int texture_id) {
+        this.c = c;
+        this.texture_id = texture_id;
+        this.height = (float) height / 2;
     }
 
-    public Node getNode(Context context){
-        Node node=new Node();
+    public Node getNode(Context context) {
+        Node node = new Node();
         node.setModel(FundamentalGenerator.getModel(context, ShadersKeeper.getProgram(ShadersKeeper.STANDARD_TEXTURE_SHADER),
-                texture_id,"Obstacle01.obj"));
+                texture_id, "Obstacle01.obj"));
         node.getRelativeTransform().setPosition(c.getPos());
-        node.getRelativeTransform().setMatrix(SFMatrix3f.getScale(c.getRadius(),height,c.getRadius()));
+        node.getRelativeTransform().setMatrix(SFMatrix3f.getScale(c.getRadius(), height, c.getRadius()));
         return node;
     }
 
 
-    public CollisionBox getBox(){
+    public CollisionBox getBox() {
         return c;
     }
 
--- app/src/main/java/game/graphics/Sky.java
+++ app/src/main/java/game/graphics/Sky.java
@@ -1,17 +1,22 @@
 package game.graphics;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
 
 import com.example.alessandro.computergraphicsexample.R;
 
 import java.util.List;
 
-import game.generators.FundamentalGenerator;
 import sfogl.integration.ArrayObject;
+import sfogl.integration.BitmapTexture;
+import sfogl.integration.Material;
 import sfogl.integration.Mesh;
 import sfogl.integration.Model;
 import sfogl.integration.Node;
 import sfogl.integration.ShadingProgram;
+import sfogl2.SFOGLTextureModel;
+import shadow.graphics.SFImageFormat;
 import shadow.math.SFMatrix3f;
 import shadow.math.SFTransform3f;
 import shadow.math.SFVertex3f;
@@ -22,7 +27,7 @@ import shadow.math.SFVertex3f;
 public class Sky {
 
     private static final int SKY_DISTANCE = 30;
-    private static final short[] indices=new short[]{3, 2, 0, 0, 1, 3};
+    private static final short[] indices = new short[]{3, 2, 0, 0, 1, 3};
 
     private ArrayObject arrayObjectPosX = new ArrayObject(
             new float[]{
@@ -97,30 +102,6 @@ public class Sky {
             },
             indices
     );
-    /*private ArrayObject arrayObjectNegY = new ArrayObject(
-            new float[]{
-                    +SKY_DISTANCE, -SKY_DISTANCE, +SKY_DISTANCE,
-                    +SKY_DISTANCE, -SKY_DISTANCE, -SKY_DISTANCE,
-                    -SKY_DISTANCE, -SKY_DISTANCE, +SKY_DISTANCE,
-                    -SKY_DISTANCE, -SKY_DISTANCE, +SKY_DISTANCE,
-                    +SKY_DISTANCE, -SKY_DISTANCE, -SKY_DISTANCE,
-                    -SKY_DISTANCE, -SKY_DISTANCE, -SKY_DISTANCE
-            },
-            new float[]{
-                    0, +1, 0
-            },
-            new float[]{
-                    1,0,0,
-                    1,1,0,
-                    0,0,0,
-                    0,0,0,
-                    1,1,0,
-                    0,1,0
-            },
-            new short[]{
-                    0,1,2,3,4,5
-            }
-    );*/
     private ArrayObject arrayObjectNegZ = new ArrayObject(
             new float[]{
                     +SKY_DISTANCE, -SKY_DISTANCE, -SKY_DISTANCE,
@@ -153,24 +134,30 @@ public class Sky {
     }
 
     private void setup() {
-        List<Node> list=mainNode.getSonNodes();
+        List<Node> list = mainNode.getSonNodes();
         list.add(generateNode(arrayObjectPosX, R.drawable.skybox_posx));
         list.add(generateNode(arrayObjectPosY, R.drawable.skybox_posy));
         list.add(generateNode(arrayObjectPosZ, R.drawable.skybox_negz));
         list.add(generateNode(arrayObjectNegX, R.drawable.skybox_negx));
-        //list.add(generateNode(arrayObjectNegY, R.drawable.skybox_negy));
         list.add(generateNode(arrayObjectNegZ, R.drawable.skybox_posz));
 
         mainNode.getRelativeTransform().setPosition(0, 0, 0);
         mainNode.getRelativeTransform().setMatrix(SFMatrix3f.getIdentity());
     }
 
-    private Node generateNode(ArrayObject arrayObject, int texture) {
+    private Node generateNode(ArrayObject arrayObject, int textureId) {
         Mesh meshPos = new Mesh(arrayObject);
         meshPos.init();
         Model modelPos = new Model();
         modelPos.setRootGeometry(meshPos);
-        modelPos.setMaterialComponent(FundamentalGenerator.getMaterial(context, program, texture));
+
+        int textureModel = SFOGLTextureModel.generateTextureObjectModel(SFImageFormat.RGB, GLES20.GL_CLAMP_TO_EDGE, GLES20.GL_CLAMP_TO_EDGE, GLES20.GL_LINEAR, GLES20.GL_LINEAR);
+        BitmapTexture bitmapTexture = BitmapTexture.loadBitmapTexture(BitmapFactory.decodeResource(context.getResources(), textureId), textureModel);
+        bitmapTexture.init();
+        Material material = new Material(program);
+        material.getTextures().add(bitmapTexture);
+
+        modelPos.setMaterialComponent(material);
         Node nodePos = new Node();
         nodePos.setModel(modelPos);
 
